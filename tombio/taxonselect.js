4d0c5478738272cf6f88ab542ca43e61f08bd20a
burkmarr <rich.burkmar@gmail.com>
2017-10-18 13:36:47 +0100

    Commit to change branches.

--- tombio/taxonselect.js
+++ tombio/taxonselect.js
@@ -4,7 +4,13 @@
 
     exports.control = function () {
 
-        var mainDiv = $('<div/>').text('Hello world!');
+
+        var mainDiv = $('<div/>');
+        mainDiv.css("background-color", "yellow");
+        var textFiler = $('<input/>').addClass("ui-widget ui-widget-content ui-corner-all");
+        textFiler.css("margin", "10");
+        mainDiv.append(textFiler)
+
         return mainDiv;
     }
 })(this.taxonselect = {})
