a425ae73075016c84547816fc8171e10be84364f
Burkmar <rburkmar@ceh.ac.uk>
2019-03-30 11:57:10 +0000

    Added some missing files.

---
+++ start.bat
@@ -0,0 +1,13 @@
+echo off
+echo.
+echo This is the command window for Identikit knowledge-base developers.
+echo Use this window to enter commands like...
+echo.
+echo    npm install (to initialise the Identikit environment after first install).
+echo    npm start (to run the Identikit).
+echo.
+echo Once the Identikit window opens, you can minimise this window,
+echo but don't close it completely because that will close Identikit.
+echo.
+set NODE_ENV=production
+cmd /k
