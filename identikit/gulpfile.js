0c2a39a22006a1e0be5af9216e00c6f9b271931d
Burkmar <rburkmar@ceh.ac.uk>
2019-04-04 17:38:24 +0100

    Updated gulp to v4 to address security vulnerabilities

---
+++ identikit/gulpfile.js
@@ -0,0 +1,47 @@
+﻿let gulp = require("gulp");
+let rename = require("gulp-rename");
+let uglify = require('gulp-uglify-es').default;
+let cleanCSS = require('gulp-clean-css');
+ 
+gulp.task("js", function () {
+    return gulp.src(['tombio/**/*.js', '!tombio/dependencies/**/*.js', '!tombio/min/js/*.js'])
+        .pipe(rename(function(path){
+            path.dirname = "";
+            path.basename += ".min";
+        }))
+        .pipe(uglify(/* options */))
+        .pipe(gulp.dest("tombio/min/js"));
+});
+
+gulp.task("onsenjs", function () {
+    //This is required because a change has been made to onsenui to generate an event
+    //when expandable list item is expanded.
+    return gulp.src('tombio/dependencies/onsenui-2.10.3/js/onsenui.js')
+        .pipe(rename(function(path){
+            path.basename += ".min";
+        }))
+        .pipe(uglify(/* options */))
+        .pipe(gulp.dest("tombio/dependencies/onsenui-2.10.3/js"));
+});
+
+gulp.task('css', function () {
+    return gulp.src(['tombio/**/*.css', '!tombio/dependencies/**/*.css', '!tombio/min/css/*.css'])
+        .pipe(rename(function(path){
+            path.dirname = "";
+            path.basename += ".min";
+        }))
+        .pipe(cleanCSS(/* options */))
+        .pipe(gulp.dest("tombio/min/css"));
+});
+
+gulp.task("onsencss", function () {
+    //This is required because a change has been made to onsenui CSS to fix scrolling issues.
+    return gulp.src('tombio/dependencies/onsenui-2.10.3/css/onsenui.css')
+        .pipe(rename(function(path){
+            path.basename += ".min";
+        }))
+        .pipe(cleanCSS(/* options */))
+        .pipe(gulp.dest("tombio/dependencies/onsenui-2.10.3/css"));
+});
+
+gulp.task('default', gulp.series('js', 'onsenjs', 'css', 'onsencss'));
